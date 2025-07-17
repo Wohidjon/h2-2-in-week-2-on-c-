faac8669947e9022ff23878d3bec3b68a5e6fb8e
WOWA <wohidhamidov@gmail.comdotnet>
2025-07-17 15:30:07 +0500

    sdmckskldmcksdmcks

--- Program.cs
+++ Program.cs
@@ -19,17 +19,17 @@
 // login1.UserLoginSystem("Abdu", "1234");
 // Console.WriteLine(login1.GetFullInfo());
 
-// Task 3
-        Author author1 = new Author();
-        Author author2 = new Author("Толстой", 50);
-        Author author3 = new Author("Пушкин", 37, "Россия");
+// // Task 3
+//         Author author1 = new Author();
+//         Author author2 = new Author("Толстой", 50);
+//         Author author3 = new Author("Пушкин", 37, "Россия");
 
-        author3.Introduce();
-        author3.CelebrateBirthday();
-        Console.WriteLine($"Новый возраст: {author3.GetAge()}");
+//         author3.Introduce();
+//         author3.CelebrateBirthday();
+//         Console.WriteLine($"Новый возраст: {author3.GetAge()}");
 
 
-// Task 4
-Country tajikistan = new Country("Tajikistan", "Dushanbe", 9537645, "Tajik");
-tajikistan.PrintInfo();
+// // Task 4
+// Country tajikistan = new Country("Tajikistan", "Dushanbe", 9537645, "Tajik");
+// tajikistan.PrintInfo();
 
